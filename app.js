a57441fadb864814e99d8af271372d30fb730409
renecas22 <70676803+renecas22@users.noreply.github.com>
2022-08-10 15:48:23 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,649 @@
+var animales = [{
+        imagen: "./imagenes/abeja.jpg",
+        nombre: "aveja",
+        traduccion: "bee",
+        audio: "./audios/abeja.ogg",
+    },
+    {
+        imagen: "./imagenes/aguila.jpg",
+        nombre: "Aguila",
+        traduccion: "Eagle",
+        audio: "./audios/aguila.ogg",
+
+    },
+    {
+        imagen: "./imagenes/araña.jpg",
+        nombre: "Araña",
+        traduccion: "spider",
+        audio: "./audios/araña.ogg",
+
+    },
+    {
+        imagen: "./imagenes/ardilla.png",
+        nombre: "ardilla",
+        traduccion: "chipmunk",
+        audio: "./audios/ardilla.ogg",
+
+    },
+    {
+        imagen: "./imagenes/ballena.jpg",
+        nombre: "ballena",
+        traduccion: "whale",
+        audio: "./audios/ballena.ogg",
+
+    },
+    {
+        imagen: "./imagenes/bisonte (2).jpg",
+        nombre: "bisonte",
+        traduccion: "bison",
+        audio:"./audios/bisonte.ogg",
+
+    },
+    {
+        imagen: "./imagenes/bufalo.jpg",
+        nombre: "búfalo",
+        traduccion: "buffalo",
+        audio: "./audios/bufalo.ogg",
+
+    },
+    {
+        imagen: "./imagenes/burro.jpg",
+        nombre: "burro",
+        traduccion: " donkey ",
+        audio:"./audios/burro.ogg",
+
+    },
+    {
+        imagen: "./imagenes/caballo.jpg",
+        nombre: "caballo",
+        traduccion: "horse",
+        audio:"./audios/caballo.ogg",
+
+    },
+    {
+        imagen: "./imagenes/camello.jpg",
+        nombre: "camello",
+        traduccion: "camel",
+        audio: "./audios/camello.ogg",
+
+    },
+    {
+        imagen: "./imagenes/canario.jpg",
+        nombre: "canario",
+        traduccion: "canary",
+        audio: "./audios/canario.ogg",
+
+    },
+    {
+        imagen: "./imagenes/cangrejo.jpg",
+        nombre: "cangrejo",
+        traduccion: "crab",
+        audio: "./audios/cangrejo.ogg",
+    },
+    {
+        imagen: "./imagenes/canguro.jpeg",
+        nombre: "canguro",
+        traduccion: "kangaroo",
+        audio: "./audios/canguro.ogg",
+
+    },
+    {
+        imagen: "./imagenes/caracol.jpg",
+        nombre: "caracol",
+        traduccion: "snail",
+        audio: "./audios/caracol.ogg",
+
+    },
+    {
+        imagen: "./imagenes/cebra.jpg",
+        nombre: "cebra",
+        traduccion: "zebra",
+        audio:  "./audios/cebra.ogg",
+
+    },
+    {
+        imagen: "./imagenes/cerdo.jpg",
+        nombre: "cerdo",
+        traduccion: "pig",
+        audio: "./audios/cerdo.ogg",
+
+    },
+    {
+        imagen: "./imagenes/chimpance.jpg",
+        nombre: "chimpancé",
+        traduccion: "chimpanzee",
+
+    },
+    {
+        imagen: "./imagenes/ciervo.jpg",
+        nombre: "ciervo",
+        traduccion: "deer",
+
+    },
+    {
+        imagen: "./imagenes/cisne.jpg",
+        nombre: "cisne",
+        traduccion: "swan",
+
+    },
+    {
+        imagen: "./imagenes/cocodrilo.jpg",
+        nombre: "cocodrilo ",
+        traduccion: "crocodile",
+
+    },
+    {
+        imagen: "./imagenes/conejo.jpg",
+        nombre: "conejo",
+        traduccion: "rabbit",
+
+    },
+    {
+        imagen: "./imagenes/elefante.jpg",
+        nombre: "elefante",
+        traduccion: "elephant",
+
+    },
+    {
+        imagen: "./imagenes/escarabajo.jpg",
+        nombre: "escarabajo",
+        traduccion: "beetle",
+
+    },
+    {
+        imagen: "./imagenes/escorpion.jpg",
+        nombre: "escorpión",
+        traduccion: "scorpion",
+
+    },
+    {
+        imagen: "./imagenes/foca.jpg",
+        nombre: "foca",
+        traduccion: " seal ",
+
+    },
+    {
+        imagen: "./imagenes/gallina.jpg",
+        nombre: "gallina",
+        traduccion: " hen ",
+
+    },
+    {
+        imagen: "./imagenes/gallo.jpg",
+        nombre: "gallo",
+        traduccion: " cock ",
+
+    },
+    {
+        imagen: "./imagenes/gato.jpg",
+        nombre: "gato",
+        traduccion: "cat",
+
+    },
+    {
+        imagen: "./imagenes/girafa.jpg",
+        nombre: "jirafa",
+        traduccion: "giraffe",
+
+    },
+    {
+        imagen: "./imagenes/golondrina.jpg",
+        nombre: "golondrina",
+        traduccion: "swallow",
+
+    },
+    {
+        imagen: "./imagenes/hiena.jpg",
+        nombre: "hiena",
+        traduccion: "hyena",
+
+    },
+    {
+        imagen: "./imagenes/hipopotamo.jpg",
+        nombre: "hipopotamo",
+        traduccion: "hippopotamus",
+
+    },
+    {
+        imagen: "./imagenes/hormiga.jpg",
+        nombre: "hormiga",
+        traduccion: "ant",
+
+    },
+    {
+        imagen: "./imagenes/javali.jpg",
+        nombre: "jabali",
+        traduccion: "wild boar",
+
+    },
+    {
+        imagen: "./imagenes/leon.jpg",
+        nombre: "leon",
+        traduccion: "Lion",
+
+    },
+    {
+        imagen: "./imagenes/leopardo.jpg",
+        nombre: "leopardo",
+        traduccion: "leopard",
+
+    },
+    {
+        imagen: "./imagenes/loro.jpg",
+        nombre: "loro",
+        traduccion: " parrot ",
+
+    },
+    {
+        imagen: "./imagenes/mono.jpg",
+        nombre: "mono",
+        traduccion: "monkey",
+
+    },
+    {
+        imagen: "./imagenes/mosca.jpg",
+        nombre: "mosca",
+        traduccion: " fly ",
+
+    },
+    {
+        imagen: "./imagenes/obeja.jpg",
+        nombre: "obeja",
+        traduccion: "sheep",
+
+    },
+    {
+        imagen: "./imagenes/oso panda.jpg",
+        nombre: "oso panda",
+        traduccion: "panda bear",
+        audio:"./audios/oso panda.ogg",
+
+    },
+    {
+        imagen: "./imagenes/oso polar.jpg",
+        nombre: "oso polar",
+        traduccion: "plar bear",
+
+    },
+    {
+        imagen: "./imagenes/oso.jpg",
+        nombre: "oso",
+        traduccion: "bear",
+
+    },
+    {
+        imagen: "./imagenes/perdiz.jpg",
+        nombre: "perdiz",
+        traduccion: "partridge",
+
+    },
+    {
+        imagen: "./imagenes/perro.jpg",
+        nombre: "perro",
+        traduccion: "dog",
+
+    },
+    {
+        imagen: "./imagenes/pez.jpg",
+        nombre: "pez",
+        traduccion: "fish",
+
+    },
+    {
+        imagen: "./imagenes/pinguino.jpg",
+        nombre: "pinguino",
+        traduccion: "penguin ",
+
+    },
+    {
+        imagen: "./imagenes/pollo.jpg",
+        nombre: "pollo",
+        traduccion: "chick",
+
+    },
+    {
+        imagen: "./imagenes/saltamontes.jpg",
+        nombre: "saltamontes",
+        traduccion: "grasshoppers",
+
+    },
+    {
+        imagen: "./imagenes/serpiente.jpg",
+        nombre: "serpiente",
+        traduccion: "snake",
+
+    },
+    {
+        imagen: "./imagenes/tiburon.jpg",
+        nombre: "tiburon",
+        traduccion: "shark",
+
+    },
+    {
+        imagen: "./imagenes/tigre.jpg",
+        nombre: "tigre",
+        traduccion: "tiger",
+
+    },
+    {
+        imagen: "./imagenes/topo.jpg",
+        nombre: "topo",
+        traduccion: "mole",
+
+    },
+    {
+        imagen: "./imagenes/toro.jpg",
+        nombre: "toro",
+        traduccion: "bull",
+
+    },
+    {
+        imagen: "./imagenes/tortuga.jpg",
+        nombre: "tortuga",
+        traduccion: "turtle",
+
+    },
+    {
+        imagen: "./imagenes/vaca.jpg",
+        nombre: "vaca",
+        traduccion: "cow",
+
+    },
+    {
+        imagen: "./imagenes/zorro.jpg",
+        nombre: "zorro",
+        traduccion: "fox"
+
+    },
+    {
+        imagen: "./imagenes/ajolote_0.jpg",
+        nombre: "ajolote",
+        traduccion: "axolotl",
+        audio: "./audios/ajolote.ogg",
+
+    },
+    {
+        imagen: "./imagenes/coballo.jpg",
+        nombre: "coballo",
+        traduccion: "coballo",
+
+    },
+    {
+        imagen: "./imagenes/cuis.jpg",
+        nombre: "cuis",
+        traduccion: "cuis",
+
+    },
+    {
+        imagen: "./imagenes/hamster.jpg",
+        nombre: "hamster",
+        traduccion: "hamster",
+
+    },
+    {
+        imagen: "./imagenes/koala.jpg",
+        nombre: "koala",
+        traduccion: "koala",
+
+    },
+    {
+        imagen: "./imagenes/pantera.jpg",
+        nombre: "pantera",
+        traduccion: "panther",
+
+    },
+    {
+        imagen: "./imagenes/avispa.jpg",
+        nombre: "avispa",
+        traduccion: "wasp",
+        audio: "./audios/avispa.ogg",
+
+    },
+    {
+        imagen: "./imagenes/zorrino.jpg",
+        nombre: "zorino",
+        traduccion:"skunk",
+
+    },
+    {
+        imagen:"./imagenes/suricata.jpg",
+        nombre: "suricata",
+        traduccion:"meerkat",
+    },
+    {
+        imagen: "./imagenes/sapo.jpg",
+        nombre: "sapo",
+        traduccion: "toad",
+
+    },
+    {
+        imagen: "./imagenes/saltamontes.jpg",
+        nombre: "saltamontes",
+        traduccion: "grasshopper",
+
+    },
+    {
+        imagen: "./imagenes/rinoceronte.jpg",
+        nombre: "rinoceronte",
+        traduccion: "rhinoceros",
+
+    },
+    {
+        imagen: "./imagenes/rata.jpg",
+        nombre: "rata",
+        traduccion: "rat",
+
+    },
+    {
+        imagen: "./imagenes/pulpo.jpg",
+        nombre: "pulpo",
+        traduccion: "octopus",
+
+    },
+    {
+        imagen: "./imagenes/puerco espin.jpg",
+        nombre: "puerco espin",
+        traduccion: "porcupine",
+
+    },
+    {
+        imagen: "./imagenes/paloma.jpg",
+        nombre: "paloma",
+        traduccion: "dove",
+
+    },
+    {
+        imagen: "./imagenes/nutria.jpg",
+        nombre: "nutria",
+        traduccion: "Otter",
+
+    },
+    {
+        imagen: "./imagenes/murcielago_redes.jpg",
+        nombre:"murcielago",
+        traduccion:"bat",
+
+    },
+    {
+        imagen: "./imagenes/mapache.jpg",
+        nombre: "mapache",
+        traduccion: "raccoon",
+
+    },
+    {
+        imagen: "./imagenes/mantarraya.jpg",
+        nombre: "mantarraya",
+        traduccion: "Stingray",
+
+    },
+    {
+        imagen: "./imagenes/luciernaga.jpg",
+        nombre: "luciernaga",
+        traduccion: "firefly",
+
+    },
+    {
+        imagen: "./imagenes/lombriz.jpg",
+        nombre: "lombriz",
+        traduccion: "worm",
+
+    },
+    {
+        imagen: "./imagenes/lobo.jpg",
+        nombre: "lobo",
+        traduccion: "wolf",
+
+    },
+    {
+        imagen: "./imagenes/lemur.jpg",
+        nombre: "lemur",
+        traduccion: "lemur",
+
+    },
+    {
+        imagen: "./imagenes/lagarto.jpg",
+        nombre: "lagarto",
+        traduccion: "lizard",
+
+    },
+    {
+        imagen: "./imagenes/lagartija.jpg",
+        nombre: "lagartija",
+        traduccion: "lizard",
+
+    },
+    {
+        imagen: "./imagenes/javali.jpg",
+        nombre: "javali",
+        traduccion: "javali",
+
+    },
+    {
+        imagen: "./imagenes/huron.jpg",
+        nombre: "huron",
+        traduccion: "ferret",
+
+    },
+    {
+        imagen: "./imagenes/gorila.jpg",
+        nombre: "gorila",
+        traduccion: "gorilla",
+
+    },
+    {
+        imagen: "./imagenes/erizo.jpg",
+        nombre: "erizo",
+        traduccion: "sea ​​urchin",
+
+    },
+    {
+        imagen: "./imagenes/dragon de komodo.jpg",
+        nombre: "dragon de komodo",
+        traduccion: "komodo dragon",
+
+    },
+    {
+        imagen: "./imagenes/delfin.jpg",
+        nombre: "delfin",
+        traduccion: "dolphin",
+
+    },
+    {
+        imagen: "../imagenes/culebra.jpg",
+        nombre: "culebra",
+        traduccion: "snake",
+
+    },
+    {
+        imagen: "./imagenes/colobri.jpg",
+        nombre: "colibri",
+        traduccion: "hummingbird",
+
+    },
+    {
+        imagen: "./imagenes/Caballitos-de-mar2.jpg",
+        nombre: "caballito de mar",
+        traduccion: "Seahorse",
+        audio: "./audios/caballito de mar.ogg",
+
+    },
+    {
+        imagen: "./imagenes/esponja de mar.jpg",
+        nombre: "esponja de mar",
+        traduccion: "sea ​​sponge",
+
+    },
+    {
+        imagen: "./imagenes/estrella de mar.jpg",
+        nombre: "estrella de mar",
+        traduccion: "sea ​​star",
+
+    },
+    {
+        imagen: "./imagenes/carpincho.jpg",
+        nombre: "carpincho",
+        traduccion: "capybara",
+        audio: "./audios/carpincho.ogg",
+
+    },
+    {
+        imagen: "./imagenes/buho.jpg",
+        nombre: "buho",
+        traduccion: "owl",
+        audio: "./audios/buho.ogg",
+        
+
+    },
+];
+
+
+// metodos de entrada
+
+let botonTraducir = document.querySelector('.button');
+botonTraducir.addEventListener('click', (e) => {
+    e.preventDefault();
+    traducir(animales);
+})
+
+let entradaDeBusqueda = document.querySelector('#input');
+entradaDeBusqueda.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        traducir(animales);
+    }
+});
+
+
+function traducir(array) {
+
+    let displayInfo = document.querySelector('.mostrarInfo');
+    let animalBuscado = entradaDeBusqueda.value.toLowerCase();
+
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        const nombres = array[i].nombre.toLowerCase();
+
+        if (animalBuscado == nombres) {
+            displayInfo.innerHTML = `
+            <div class="tarjetas">
+            <div class="card" style="width: 18rem;">
+              <img src="${element.imagen}" class="card-img-top" alt="...">
+             <div class="card-body">
+                <h3>Español : ${element.nombre}</h3>
+                <h3>Ingles  : ${element.traduccion}</h3>
+                <audio src="${element.audio}" controls></audio>
+               </div>
+            </div>
+            </div>
+            `
+            console.log(nombres);
+            break;
+        } else {
+            displayInfo.innerHTML = `<h1>el ${animalBuscado} no se encontro</h1>`
+        
+
+        }
+
+
+    }
+}
